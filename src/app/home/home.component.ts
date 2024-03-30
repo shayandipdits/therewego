19e717087bd190be9f5e1a35e8c4427dbf07146f
KGN <shayandip.dits@gmail.com>
2024-03-30 11:10:26 +0530

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselModule,CommonModule],
+  imports: [CarouselModule,CommonModule,NgbModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  starRating = 0;
   customOptions: any = {
     loop: true,
     mouseDrag: true,
