9f6f465842c32ac1e9115813a02c0b5a7f7b19b3
KGN <shayandip.dits@gmail.com>
2024-04-01 01:55:02 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServiceComponent } from './service/service.component';
 import { ServiceDetailsComponent } from './service-details/service-details.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 export const routes: Routes = [
     {path:'' , component:HomeComponent},
     {path:'about-us' , component:AboutUsComponent},
     {path:'service' , component:ServiceComponent},
-    {path:'service-details' , component:ServiceDetailsComponent}
+    {path:'service-details' , component:ServiceDetailsComponent},
+    {path:'how-it-works' , component:HowItWorksComponent}
 ];
