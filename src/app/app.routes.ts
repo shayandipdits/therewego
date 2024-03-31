a06a8986b075de32ac7857cca282a122e8f3e417
KGN <shayandip.dits@gmail.com>
2024-03-31 14:23:21 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ServiceComponent } from './service/service.component';
 
 export const routes: Routes = [
     {path:'' , component:HomeComponent},
-    {path:'about-us' , component:AboutUsComponent}
+    {path:'about-us' , component:AboutUsComponent},
+    {path:'service' , component:ServiceComponent}
 ];
