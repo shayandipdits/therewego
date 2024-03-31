11710199ba5ec0de5dd9061e7796982e83db017f
KGN <shayandip.dits@gmail.com>
2024-04-01 00:51:23 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServiceComponent } from './service/service.component';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
 
 export const routes: Routes = [
     {path:'' , component:HomeComponent},
     {path:'about-us' , component:AboutUsComponent},
-    {path:'service' , component:ServiceComponent}
+    {path:'service' , component:ServiceComponent},
+    {path:'service-details' , component:ServiceDetailsComponent}
 ];
--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
