9502d24629945efb0b3f2dafce215b5dda20442f
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-04-01 12:38:35 +0530

    updated

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {ServicesService} from "../services/services.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf, NgForOf],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
 export class ServiceComponent {
+  public FILE_URL = environment.FILE_URL;
   serviceList: any[] = [];
 
   constructor(private servicesService:ServicesService) {
