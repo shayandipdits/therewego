3940d3d908574332a8c8047a8b2948fc71c6bb12
KGN <shayandip.dits@gmail.com>
2024-04-02 10:25:18 +0530

    updated

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router'
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
