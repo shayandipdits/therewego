2f96025f45a40676da782552a9d0205b798fbd77
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-04-05 18:53:16 +0530

    updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  BASE_API_URL: 'https://devanthosting.cloud/togethervgo/thereVGo_api/api',
-  FILE_URL: 'https://devanthosting.cloud/togethervgo/public/',
+  production: false,
+  BASE_API_URL: 'http://localhost/thereVGo_api/api',
+  FILE_URL: 'http://localhost/thereVGo_api/public/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   // BASE_API_URL: 'http://localhost/thereVGo_api/api',
   // FILE_URL: 'http://localhost/thereVGo_api/public/',
   BASE_API_URL: 'https://devanthosting.cloud/togethervgo/thereVGo_api/api',
-  FILE_URL: 'https://devanthosting.cloud/togethervgo/public/',
+  FILE_URL: 'https://devanthosting.cloud/togethervgo/thereVGo_api/public/',
 };
