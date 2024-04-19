3e50a13ddd8a30ef19b8a327332ff3d98877ba95
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-04-19 10:57:02 +0530

    updated

---
+++ src/environments/server.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // BASE_API_URL: 'http://localhost/thereVGo_api/api',
+  // FILE_URL: 'http://localhost/thereVGo_api/public/',
+  BASE_API_URL: 'https://devanthosting.cloud/togethervgo/thereVGo_api/public/api',
+  FILE_URL: 'https://devanthosting.cloud/togethervgo/thereVGo_api/public/',
+};
